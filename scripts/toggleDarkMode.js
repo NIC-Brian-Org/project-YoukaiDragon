f05839b264a5a591cb59df7a3814f6c424e824e6
Tyson <tyson.foster@hotmail.com>
2022-03-22 18:28:58 -0700

    implement dark/light mode toggle

--- scripts/toggleDarkMode.js
+++ scripts/toggleDarkMode.js
@@ -1,1 +1,27 @@
 'use strict';
+
+let el = document.getElementById("darkModeToggle");
+el.addEventListener("click",toggleDarkMode);
+el.addEventListener("mouseenter", highlightToggle);
+el.addEventListener("mouseout", stopHighlight);
+
+function toggleDarkMode() {
+  if(el.textContent == "Dark Mode"){
+    //switch to light mode
+    document.body.id = "lightMode";
+    el.textContent = "Light Mode";
+  } else {
+    //switch to dark mode
+    document.body.id = "darkMode";
+    el.textContent = "Dark Mode";
+  }
+}
+
+function highlightToggle() {
+  el.style.color = "orange";
+  el.style.cursor = "pointer";
+}
+
+function stopHighlight() {
+  el.style.color = "black";
+}
